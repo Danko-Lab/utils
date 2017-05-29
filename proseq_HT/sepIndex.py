be2d5771ae7685d2501a8b9f1d9b7cd3c736bd26
Charles <dankoc@gmail.com>
2017-05-29 18:52:00 -0400

    Trims 3-prime UMI and J-barcode. Discard reads less than 25 nt.

--- proseq_HT/sepIndex.py
+++ proseq_HT/sepIndex.py
@@ -33,6 +33,12 @@ fastq2 = {idx: gzip.open(out_prefix+'_'+idx+'_R2.fastq.gz', 'wb') for idx in idx
 r1=open(in_fastq_R1)
 r2=open(in_fastq_R2)
 
+## Reverse complement function.
+def revComp(seq):
+	seq_dict = {'A':'T','T':'A','G':'C','C':'G','N':'N'}
+	return "".join([seq_dict[base] for base in reversed(seq)])
+
+
 while True:
 	## File 1 
 	r1_name = r1.readline()
@@ -53,7 +59,18 @@ while True:
 	## Then record the entry!
 	total += 1
 
-	## ID idx
+	#####################################################################################
+	## Trim 3' end of each read, if necessary because we have read through entire read 
+	## and UMI is repeated on 3' end.
+
+	## Does the reverse complement of the final 4 bases of R1 equal the first 4 of R2?
+	## This code works iff adapters have already been clipped, but then should be pretty robust.
+	if revComp(r1_seq[-5:-1]) == r2_seq[:4] and revComp(r2_seq[-11:-1]) == r1_seq[:10]: ## The final character in the line is the newline character, so -5:-1.
+		r1_seq = r1_seq[:-5] + "\n"
+		r2_seq = r2_seq[:-11] + "\n"
+
+	##############################################
+	## Separate distinct 5' barcodes.  ID idx.
 	try:
 		fastq1[r1_seq[i1:i2]].write(r1_name+r1_seq[trimLeft:]+r1_plus+r1_qual[trimLeft:])
                 fastq2[r1_seq[i1:i2]].write(r2_name+r2_seq[trimRight:]+r2_plus+r2_qual[trimRight:])
@@ -61,19 +78,26 @@ while True:
 		counts[fileidx] += 1
 
 	except (KeyError, ValueError) as e:
-		try:
+		try:       ## Off by one means can remove 1 bp from trimLeft (index expected to be 1 bp shorter b/c of indel).
 			fastq1[r1_seq[(i1-1):(i2-1)]].write(r1_name+r1_seq[(trimLeft-1):]+r1_plus+r1_qual[(trimLeft-1):])
-			fastq2[r1_seq[(i1-1):(i2-1)]].write(r2_name+r2_seq[(trimRight-1):]+r2_plus+r2_qual[(trimRight-1):])
+			fastq2[r1_seq[(i1-1):(i2-1)]].write(r2_name+r2_seq[trimRight:]+r2_plus+r2_qual[trimRight:])
 			fileidx = idxids.index(r1_seq[(i1-1):(i2-1)])
 			counts[fileidx] += 1
 
 		except (KeyError, ValueError) as e:
 			try:
 				obs_idx = r1_seq[i1:i2]  ## Add back likely 5-mers in the TCGTA index.
+
 				if obs_idx == 'CGTAG' or obs_idx == 'TGTAG' or obs_idx == 'TCGAG' or obs_idx == 'TCGTG' or obs_idx == 'TCTAG': 
 					fastq1['TCGTA'].write(r1_name+r1_seq[trimLeft:]+r1_plus+r1_qual[trimLeft:])
 					fastq2['TCGTA'].write(r2_name+r2_seq[trimRight:]+r2_plus+r2_qual[trimRight:])
 					fileidx = idxids.index('TCGTA')
+					counts[fileidx] += 1
+
+				elif obs_idx == 'TGCAG' or obs_idx == 'ATGAG':
+					fastq1['ATGCA'].write(r1_name+r1_seq[trimLeft:]+r1_plus+r1_qual[trimLeft:])
+					fastq2['ATGCA'].write(r2_name+r2_seq[trimRight:]+r2_plus+r2_qual[trimRight:])
+					fileidx = idxids.index('ATGCA')
 					counts[fileidx] += 1
 
 				else:

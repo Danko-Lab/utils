29b8f3a3e55258ef40a1d01fb6619b7342577a94
shaopei <sc2457@cornell.edu>
2019-02-04 16:41:16 -0500

    updat

--- proseq_HT/sepIndex.py
+++ proseq_HT/sepIndex.py
@@ -14,7 +14,7 @@ print "Fastq files:   " + in_fastq_R1 + " " + in_fastq_R2
 print "Output prefix: " + out_prefix
 
 ## http://stackoverflow.com/questions/29550290/how-to-open-a-list-of-files-in-python
-idxids    = ['ATGCA', 'TCGTA', 'CGATA', 'GACGA', 'GCACA', 'TGCAA', 'CATGA', 'GTCAA', 'AGTCA', 'TACTA', 'CTAGA', 'GCTGA']
+idxids    = ['ATGCA', 'TCGTA', 'CGATA', 'GACGA', 'GCACA', 'TGCAA', 'CATGA', 'GTCAA', 'AGTCA', 'TACTA', 'CTAGA', 'GCTGA', 'NODEX']
 counts    = [0] * len(idxids)
 discard   = 0
 total     = 0
@@ -100,11 +100,25 @@ while True:
 					fileidx = idxids.index('ATGCA')
 					counts[fileidx] += 1
 
+				elif obs_idx == 'GACGT':
+					fastq1['GACGA'].write(r1_name+r1_seq[trimLeft:]+r1_plus+r1_qual[trimLeft:])
+					fastq2['GACGA'].write(r2_name+r2_seq[trimRight:]+r2_plus+r2_qual[trimRight:])
+					fileidx = idxids.index('GACGA')
+					counts[fileidx] += 1
+
 				else:
 					discard += 1
+					fastq1['NODEX'].write(r1_name+r1_seq[trimLeft:]+r1_plus+r1_qual[trimLeft:])
+					fastq2['NODEX'].write(r2_name+r2_seq[trimRight:]+r2_plus+r2_qual[trimRight:])
+					fileidx = idxids.index('NODEX')
+					counts[fileidx] += 1
 
 			except (KeyError, ValueError) as e:
 				discard +=1
+				fastq1['NODEX'].write(r1_name+r1_seq[trimLeft:]+r1_plus+r1_qual[trimLeft:])
+				fastq2['NODEX'].write(r2_name+r2_seq[trimRight:]+r2_plus+r2_qual[trimRight:])
+				fileidx = idxids.index('NODEX')
+				counts[fileidx] += 1
 				continue
 
 
